958e2e822413f639bf0292a9cbfde630c4cc3196
ctllin <guolinit@163.com>
2017-06-01 14:50:44 +0800

    pdfOnLineRead
    pdfOnLineRead

---
+++ src/dfd.java
@@ -0,0 +1,17 @@
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+
+public class dfd {
+
+	/**
+	 * @param args
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		// TODO Auto-generated method stub
+System.out.println(URLEncoder.encode("ÄãºÃ","utf-8"));
+	}
+
+}
